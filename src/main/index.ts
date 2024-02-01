43332084e93ad2577a020c94a659cde312d5a99e
Nader Ali Fares <naderalifares16@hotmail.com>
2024-01-31 23:25:36 -0300

    Creating RootLayout Component, Sidebar and Content Areas

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,6 +13,11 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     center: true,
     title: 'MD Notes',
+    frame: false,
+    vibrancy: 'under-window',
+    visualEffectState: 'active',
+    titleBarStyle: 'hidden',
+    trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
