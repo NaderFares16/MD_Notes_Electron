28a62754f7910696407fc2e419a200df41373d42
Nader Ali Fares <naderalifares16@hotmail.com>
2024-02-01 21:37:52 -0300

    Create and Delete Buttons already running.

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,6 +1,6 @@
-import { atom } from 'jotai'
 import { NoteInfo } from '@shared/models'
-import { notesMock } from '@/store/mocks'
+import { atom } from 'jotai'
+import { notesMock } from './mocks'
 
 export const notesAtom = atom<NoteInfo[]>(notesMock)
 
@@ -10,7 +10,7 @@ export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
 
-  if (!selectedNoteIndex == null) return null
+  if (selectedNoteIndex == null) return null
 
   const selectedNote = notes[selectedNoteIndex]
 
@@ -18,4 +18,32 @@ export const selectedNoteAtom = atom((get) => {
     ...selectedNote,
     content: `Hello from Note ${selectedNoteIndex}`
   }
+})
+
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const title = `Note ${notes.length + 1}`
+
+  const newNote: NoteInfo = {
+    title,
+    lastEditTime: Date.now()
+  }
+
+  set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+
+  set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) return
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.title !== selectedNote.title)
+  )
+
+  set(selectedNoteIndexAtom, null)
 })
